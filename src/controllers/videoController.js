f37af27d2ac5373660a4b73c0f7c82c53516ab7b
Kyunghyun Koo <kyunghyunkoo@MacBook-Pro.local>
2021-09-14 21:17:00 +0900

    Add pug things

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,9 @@
-export const trending = (req, res) => res.send("Trending");
-export const see = (req, res) => res.send("See");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    res.send("Delete Video");
+}
 export const upload = (req, res) => res.send("Upload");
